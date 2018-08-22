2b46ddd64b9c1d9080fc5c0d5a5b10d5a1cf074c
Karen Zhao <karen.zhao18@gmail.com>
2018-08-22 14:21:01 -0400

    create mutation, add

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,17 @@
-import { getMovies, getById } from './db';
+import { getMovies, getById, deleteMovie, addMovie } from './db';
 
 const resolvers = {
     Query: {
         movies: () => getMovies(),
         movie: (obj, { id }) => getById(id) 
         // aka args.id cuz obj destructuring
+    },
+    Mutation: {
+        addMovie: (_, { name, score }) => addMovie(name, score)
     }
 };
 
 export default resolvers;
 
-// DOUBLE QUOTES if const resolvers = { Query: { person: () => "dokinqs" } }
+// DOUBLE QUOTES for graphql
+//  if const resolvers = { Query: { person: () => "dokinqs" } }
