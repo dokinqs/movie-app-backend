3e84323e7c60d6466673ae498b6a94eae35615a3
Karen Zhao <karen.zhao18@gmail.com>
2018-08-23 12:42:11 -0400

    refactor to axios and async, get one and all

--- graphql/db.js
+++ graphql/db.js
@@ -1,45 +1,33 @@
-import fetch from "node-fetch";
-const API_URL="https://yts.am/api/v2/list_movies.json?";
+import axios from "axios";
 
-export const getMovies = (limit, rating) => {
-    let REQUEST_URL = API_URL;
-    if (limit > 0 ) {
+const LIST_MOVIES_URL = "https://yts.am/api/v2/list_movies.json?";
+const MOVIE_DETAILS_URL = "https://yts.am/api/v2/movie_details.json";
+
+export const getMovies = async (limit, rating) => {
+    let REQUEST_URL = LIST_MOVIES_URL;
+    if (limit > 0) {
         REQUEST_URL += `limit=${limit}`;
     }
-    if (rating > 0 ) {
+    if (rating > 0) {
         REQUEST_URL += `&minimum_rating=${rating}`;
     }
-    return fetch(REQUEST_URL)
-            .then(res => res.json())
-            .then(json => json.data.movies);
-};
+    const {
+        data: {
+            data: { movies }
+        }
+    } = await axios(REQUEST_URL);
+    return movies;
+}
 
-
-// export const getMovies = () => movies;
-
-// export const getById = id => {
-//     const filteredMovies = movies.filter(movie => id === movie.id);
-//     return filteredMovies[0];
-// };
-
-// export const deleteMovie = id => {
-//     const cleanedMovies = movies.filter(movie => id !== movie.id);
-//     if (movies.length > cleanedMovies.length) {
-//         // was a deletion, assign movie to new filtered array
-//         movies = cleanedMovies;
-//         return true;
-//     } else {
-//         return false;
-//     }
-// }
-
-// export const addMovie = (name, score) => {
-//     const newMovie = {
-//         // not good id cuz if only one undeleted movie left id would be 2 even if undeleted movie had id of 3 
-//         id: `${movies.length + 1}`,
-//         name,
-//         score
-//     }
-//     movies.push(newMovie);
-//     return newMovie;
-// }
+export const getMovie = async id => {
+    const {
+        data: {
+            data: { movie }
+        }
+    } = await axios(MOVIE_DETAILS_URL, {
+        params: {
+            movie_id: id
+        }
+    })
+    return movie;
+}
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,23 +1,10 @@
-import { getMovies } from './db';
+import { getMovies, getMovie } from './db';
 
 const resolvers = {
     Query: {
-        movies: (_, { limit, rating }) => getMovies(limit, rating)
+        movies: (_, { limit, rating }) => getMovies(limit, rating),
+        movie: (_, { id }) => getMovie(id)
     }
 };
 
 export default resolvers;
-
-    // , getById, deleteMovie, addMovie 
-    
-        // movie: (obj, { id }) => getById(id) 
-        // aka args.id cuz obj destructuring
-    // ,
-    // Mutation: {
-    //     addMovie: (_, { name, score }) => addMovie(name, score),
-    //     deleteMovie: (_, { id }) => deleteMovie(id)
-    // }
-
-
-// DOUBLE QUOTES for graphql
-//  if const resolvers = { Query: { person: () => "dokinqs" } }
